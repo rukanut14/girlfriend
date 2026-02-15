28c6777d4292db0276816476a3e10d4639009600
rukanut14 <t1doran043@gmail.com>
2026-02-15 02:28:07 +0000

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,117 @@
+const answers_no = {
+    english: [
+        "No",
+        "Are you sure?",
+        "Are you really sure??",
+        "Are you really realy sure???",
+        "Think again?",
+        "Please say yes",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "Press YES mary",
+        "Ok now this is hurting my feelings!",
+        "You are now just being mean!",
+        "I'll get you a new skin if you say yes",
+        "Please give me a chance!",
+        "I am begging you!",
+        "Ok, Let's just start over.."
+    ],
+};
+
+answers_yes = {
+    "english": "Yes",
+}
+
+let language = "english"; // Default language is English
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+    if (clicks === 0) {
+        banner.src = "public/images/no.gif";
+        refreshBanner();
+    }
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers_no[language].length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers_no[language][i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers_no[language][i]);
+        i = 1;
+        no_button.innerHTML = answers_no[language][0];
+        yes_button.innerHTML = answers_yes[language];
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "public/images/yes.gif";
+    refreshBanner();
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+});
+
+function refreshBanner() {
+    // Reload banner gif to force load  
+    let banner = document.getElementById('banner');
+    let src = banner.src;
+    banner.src = '';
+    banner.src = src;
+}
+
+function changeLanguage() {
+    const selectElement = document.getElementById("language-select");
+    const selectedLanguage = selectElement.value;
+    language = selectedLanguage;
+
+    // Update question heading
+    const questionHeading = document.getElementById("question-heading");
+    if (language === "french") {
+        questionHeading.textContent = "Tu veux être mon valentin?";
+    } else if (language === "thai") {
+        questionHeading.textContent = "คืนดีกับเราได้อ่ะป่าว?";
+    } else {
+        questionHeading.textContent = "Will you be my valentine?";
+    }
+
+    // Reset yes button text
+    yes_button.innerHTML = answers_yes[language];
+
+    // Reset button text to first in the new language
+    if (clicks === 0) {
+        no_button.innerHTML = answers_no[language][0];
+    } else {
+        no_button.innerHTML = answers_no[language][clicks];
+    }
+
+    // Update success message
+    const successMessage = document.getElementById("success-message");
+    if (language === "french") {
+        successMessage.textContent = "Yepppie, à bientôt :3";
+    } else if (language === "thai") {
+        successMessage.textContent = "ฮูเร่ คืนดีกันแล้วน้า :3";
+    } else {
+        successMessage.textContent = "Yepppie, see you sooonnn :3";
+    }
+}
